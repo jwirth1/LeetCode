5722fb2dd4a02875f9ad770b290d4218c2f36645
Jimmy Wirth <jimmybean98@gmail.com>
2023-10-16 16:03:44 -0400

    Time: 9 ms (91.13%), Space: 55 MB (66.90%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        
+        return false;
+    }
+}
